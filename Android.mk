96bac22c55d72557091fa11b8906f2b69292eea0
ripee <ripee.dev@gmail.com>
2018-01-21 22:36:16 -0500

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -23,7 +23,7 @@
 # *not* include it on all devices, so it is safe even with hardware-specific
 # components.
 
-ifneq ($(filter gts210wifi gts210wifixx,$(TARGET_DEVICE)),)
+ifneq ($(filter i9100,$(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
