708a05d5b2c60325940f85fb5bd7df6435ff85e5
ripee <ripee.dev@gmail.com>
2018-01-21 22:42:40 -0500

    Update omni_gts210wifi.mk

--- omni_gts210wifi.mk
+++ omni_gts210wifi.mk
@@ -1,8 +1,8 @@
 # Release name
-PRODUCT_RELEASE_NAME := gts210wifi
+PRODUCT_RELEASE_NAME := i9100
 
 # Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
@@ -12,7 +12,7 @@ PRODUCT_COPY_FILES += \
     bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := gts210wifi
-PRODUCT_NAME := omni_gts210wifi
+PRODUCT_DEVICE := i9100
+PRODUCT_NAME := omni_i9100
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
